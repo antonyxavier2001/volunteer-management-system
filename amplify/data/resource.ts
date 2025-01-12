5d693788ad98b6b3c7544900f1c3ff0c647f3e32
Zishan <55238388+Aman-zishan@users.noreply.github.com>
2025-01-12 21:24:56 +0530

    feat: update lambda timeout

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,105 +1,109 @@
-import { type ClientSchema, a, defineData, defineFunction } from "@aws-amplify/backend";
+import {
+	type ClientSchema,
+	a,
+	defineData,
+	defineFunction,
+} from '@aws-amplify/backend';
 
 const envValues = {
-    ATLAS_CONNECTION_STRING: process.env.ATLAS_CONNECTION_STRING!,
-    COLLECTION_NAME: process.env.COLLECTION_NAME!,
-    DB_NAME: process.env.DB_NAME!
+	ATLAS_CONNECTION_STRING: process.env.ATLAS_CONNECTION_STRING!,
+	COLLECTION_NAME: process.env.COLLECTION_NAME!,
+	DB_NAME: process.env.DB_NAME!,
 };
 const listTodoHandler = defineFunction({
-  entry: './listTodo.ts',
-  environment: {
-      ...envValues,
-  }
+	entry: './listTodo.ts',
+	timeoutSeconds: 60,
+	environment: {
+		...envValues,
+	},
 });
 
 const addTodoHandler = defineFunction({
-  entry: './addTodo.ts',
-  environment: {
-      ...envValues,
-  }
+	entry: './addTodo.ts',
+	timeoutSeconds: 60,
+	environment: {
+		...envValues,
+	},
 });
 
 const deleteTodoHandler = defineFunction({
-  entry: './deleteTodo.ts',
-  environment: {
-      ...envValues,
-  }
+	entry: './deleteTodo.ts',
+	timeoutSeconds: 60,
+	environment: {
+		...envValues,
+	},
 });
 
 const updateTodoHandler = defineFunction({
-  entry: './updateTodo.ts',
-  environment: {
-      ...envValues,
-  }
+	entry: './updateTodo.ts',
+	timeoutSeconds: 60,
+	environment: {
+		...envValues,
+	},
 });
 
 const schema = a.schema({
-  Todo: a.customType({
-      _id: a.id().required(),
-      content: a.string().required(),
-    }),
-  ListTodoResponse: a.customType({
-    statusCode:a.string(),
-    todoList:a.ref("Todo").array(),
-  }),
-  AddTodoResponse: a.customType({
-    statusCode:a.string(),
-    todo:a.ref("Todo"),
-  }),
-  DeleteTodoResponse: a.customType({
-    statusCode:a.string(),
-    count:a.string(),
-  }),
-  UpdatedTodoResponse: a.customType({
-    statusCode:a.string(),
-    count:a.string(),
-  }),
-  addTodo: a
-    .mutation()
-    .arguments({
-      _id: a.id(),
-      content: a.string().required(),
-    })
-    .returns(a.ref("AddTodoResponse"))
-    .authorization(allow => [allow.authenticated()])
-    .handler(a.handler.function(addTodoHandler)),
-  listTodo: a
-    .query()
-    .returns(a.ref("ListTodoResponse"))
-    .authorization(allow => [allow.authenticated()])
-    .handler(a.handler.function(listTodoHandler)),
-  deleteTodo: a
-    .mutation()
-    .arguments({
-      _id: a.string().required(),
-    })
-    .returns(a.ref("DeleteTodoResponse"))
-    .authorization(allow => [allow.authenticated()])
-    .handler(
-      a.handler.function(deleteTodoHandler)
-    ),
-  updateTodo: a
-    .mutation()
-    .arguments({
-      _id: a.string().required(),
-      content: a.string().required(),
-    })
-    .returns(a.ref("UpdatedTodoResponse"))
-    .authorization(allow => [allow.authenticated()])
-    .handler(
-      a.handler.function(updateTodoHandler)
-    ),
-
+	Todo: a.customType({
+		_id: a.id().required(),
+		content: a.string().required(),
+	}),
+	ListTodoResponse: a.customType({
+		statusCode: a.string(),
+		todoList: a.ref('Todo').array(),
+	}),
+	AddTodoResponse: a.customType({
+		statusCode: a.string(),
+		todo: a.ref('Todo'),
+	}),
+	DeleteTodoResponse: a.customType({
+		statusCode: a.string(),
+		count: a.string(),
+	}),
+	UpdatedTodoResponse: a.customType({
+		statusCode: a.string(),
+		count: a.string(),
+	}),
+	addTodo: a
+		.mutation()
+		.arguments({
+			_id: a.id(),
+			content: a.string().required(),
+		})
+		.returns(a.ref('AddTodoResponse'))
+		.authorization((allow) => [allow.authenticated()])
+		.handler(a.handler.function(addTodoHandler)),
+	listTodo: a
+		.query()
+		.returns(a.ref('ListTodoResponse'))
+		.authorization((allow) => [allow.authenticated()])
+		.handler(a.handler.function(listTodoHandler)),
+	deleteTodo: a
+		.mutation()
+		.arguments({
+			_id: a.string().required(),
+		})
+		.returns(a.ref('DeleteTodoResponse'))
+		.authorization((allow) => [allow.authenticated()])
+		.handler(a.handler.function(deleteTodoHandler)),
+	updateTodo: a
+		.mutation()
+		.arguments({
+			_id: a.string().required(),
+			content: a.string().required(),
+		})
+		.returns(a.ref('UpdatedTodoResponse'))
+		.authorization((allow) => [allow.authenticated()])
+		.handler(a.handler.function(updateTodoHandler)),
 });
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "userPool",
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
-  },
+	schema,
+	authorizationModes: {
+		defaultAuthorizationMode: 'userPool',
+		apiKeyAuthorizationMode: {
+			expiresInDays: 30,
+		},
+	},
 });
