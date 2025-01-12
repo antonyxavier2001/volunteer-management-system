63915804a7968536d62fad4b716e71c13b30100c
Zishan <55238388+Aman-zishan@users.noreply.github.com>
2025-01-12 21:50:32 +0530

    fix: cache DB connection client

--- amplify/data/mdbUtils.ts
+++ amplify/data/mdbUtils.ts
@@ -1,16 +1,20 @@
-import {Collection, Db, MongoClient} from "mongodb";
-
+import { Collection, Db, MongoClient } from 'mongodb';
 
 // Environment variables
 const ATLAS_CONNECTION_STRING = process.env.ATLAS_CONNECTION_STRING as string;
 const COLLECTION_NAME = process.env.COLLECTION_NAME as string;
 const DB_NAME = process.env.DB_NAME as string;
 
+let cachedClient: [MongoClient, Db, Collection] | null = null;
 
 export function connectToMongodb(): [MongoClient, Db, Collection] {
-
-    const client = new MongoClient(ATLAS_CONNECTION_STRING);
-    const db = client.db(DB_NAME);
-    const collection = db.collection(COLLECTION_NAME);
-    return [client, db, collection];
+	if (cachedClient) {
+		return cachedClient;
+	} else {
+		const client = new MongoClient(ATLAS_CONNECTION_STRING);
+		const db = client.db(DB_NAME);
+		const collection = db.collection(COLLECTION_NAME);
+		cachedClient = [client, db, collection];
+		return [client, db, collection];
+	}
 }
