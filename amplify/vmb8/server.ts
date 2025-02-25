2eeec76acbdfd5b398a8f0c11555e1f2bf0d814a
Ravi Kumar <mrkravi03@gmail.com>
2025-02-25 18:23:46 +0530

    updated added only vmb8

--- amplify/vmb8/server.ts
+++ amplify/vmb8/server.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './db/db';
 import userRoutes from './routes/userRoutes';
-import adminRouter from "./routes/admin.router";
 
 
 dotenv.config();
@@ -18,7 +17,6 @@ connectDB();
 
 
 app.use('/', userRoutes);
-app.use("/admin", adminRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
